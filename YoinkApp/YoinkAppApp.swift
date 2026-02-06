7d376a6738268bbbfb771aef7632c60552c21cad
Ali Hassan <elly.hacen@icloud.com>
2026-02-06 17:01:42 +0500

    Initial Commit

---
+++ YoinkApp/YoinkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoinkAppApp.swift
+//  YoinkApp
+//
+//  Created by Ali Hassan on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct YoinkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
